d6b7389113133245802d2e038f96d8eeaeaf7ae0
Mitchell R <root@mitchellr.info>
2026-06-20 22:52:49 +0200

    ci: propagate tag versions in releases

--- cli/cmd/version.go
+++ cli/cmd/version.go
@@ -3,7 +3,7 @@ package cmd
 import "fmt"
 
 // Version is the CLI version string.
-const Version = "0.0.1"
+var Version = "0.0.1"
 
 // RunVersion prints the version and returns exit code.
 func RunVersion(args []string) int {
